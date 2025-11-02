cd21988cd1fa9f17457cc5c69b4d7bd0e7ebdeec
KovalskiWG <akipayk@inbox.ru>
2025-11-02 22:32:38 +0300

    feat/project-8 убрал баг с решения конфликтов

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-<<<<<<< HEAD
   
-=======
-
->>>>>>> main
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-<<<<<<< HEAD
       '@': path.resolve(__dirname, './src'), 
       '@app': path.resolve(__dirname, './src/app'),
       '@widgets': path.resolve(__dirname, './src/widgets'), 
       '@shared': path.resolve(__dirname, './src/shared'), 
       '@pages': path.resolve(__dirname, './src/pages')
-=======
-      '@/*': path.resolve(__dirname, './src'), 
-      '@widgets/*': path.resolve(__dirname, './src/widgets'), 
-      '@shared/*': path.resolve(__dirname, './src/shared'), 
->>>>>>> main
     }
   }
 })
